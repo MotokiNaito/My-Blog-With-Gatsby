92c88cb269fd82842ad6b58be0a082d130d6181f
Motoki Naito <motoki.developer@gmail.com>
2019-04-06 16:09:38 -0700

    add styled components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,42 +1,19 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+const HeaderContainer = styled.div`
+  background-color: #ccc;
+`
+
+const Header = () => (
+  <HeaderContainer>
+    <header>
+      <h1>
+        <Link to="/">Motoki's Blog</Link>
       </h1>
-    </div>
-  </header>
+    </header>
+  </HeaderContainer>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
